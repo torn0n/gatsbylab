c5a77d525eec7963020896a7e274b6440f9d48bc
Thomas Ornon <32384667+torn0n@users.noreply.github.com>
2019-04-22 00:16:57 +0200

    test codesandbox

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,7 +9,7 @@ const IndexPage = () => (
   <Layout>
     <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
     <h1>Hello i am Thomas Ornon</h1>
-    <p>Welcome to your new Gatsby site.</p>
+    <p>Test on codesandbox</p>
     <p>Now go build something great.</p>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
